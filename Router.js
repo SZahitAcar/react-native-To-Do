942f71ce51c5f3f18d8ffe68e4a88c828f34c51c
SZahitAcar <zahitacar@yandex.com>
2022-03-17 13:47:48 +0300

    initializing tab bar

---
+++ Router.js
@@ -0,0 +1,56 @@
+import {View, Image} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import AddToDo from './src/pages/AddToDo';
+import ToDoList from './src/pages/ToDoList';
+
+const Tab = createBottomTabNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{headerShown: false}}
+        tabBarOptions={{showLabel: false, style: {backgroundColor: '#FFE0D6'}}}>
+        <Tab.Screen
+          name="Add To Do"
+          component={AddToDo}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                <Image
+                  source={require('./assets/icons/addButton/2x/addButton2x.png')}
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#FFBD52' : '#000',
+                  }}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="To Do List"
+          component={ToDoList}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                <Image
+                  source={require('./assets/icons/agendaButton2x/2x/agenda2x.png')}
+                  style={{
+                    width: 35,
+                    height: 35,
+                    tintColor: focused ? '#FFBD52' : '#000',
+                  }}
+                />
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/pages/AddToDo/AddToDo.js
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+const AddToDo = () => {
+  return (
+    <SafeAreaView>
+      <Text>AddToDo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default AddToDo;
